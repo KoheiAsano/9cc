cfde31ba6e4ff2737085108add649a912a324f7a
KoheiAsano <komerumeru@i.softbank.jp>
2019-02-23 14:05:56 +0900

    Implement local variable of one charactor, semicolon

--- 9cc.c
+++ 9cc.c
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
 enum{
   TK_NUM = 256, // Number
+  TK_IDENT,
   TK_EOF, // End Of File
 };
 
 typedef struct {
   int ty; // Type of Token
   int val; // value when ty == TK_NUM
+  char name; //value when ty == TK_IDENT
   char *input;
 } Token;
 
@@ -20,6 +25,13 @@ typedef struct {
   int len;
 } Vector;
 
+Vector *tokens;
+int pos = 0;
+// func to report error
+void error(int i){
+  fprintf(stderr, "unknown string: '%s'\n",((Token *)tokens->data[i])->input);
+  exit(1);
+}
 Vector *new_vector() {
   Vector *vec = malloc(sizeof(Vector));
   vec->data = malloc(sizeof(void *) * 16);
@@ -36,8 +48,7 @@ void vec_push(Vector *vec, void *elem){
   vec->data[vec->len++] = elem;
 }
 
-Vector *tokens;
-int pos = 0;
+
 
 Token *add_token(Vector *v, int ty, char *input){
   Token *t = malloc(sizeof(Token));
@@ -57,7 +68,7 @@ Vector *tokenize(char *p){
       p++;
       continue;
     }
-    if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
+    if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '=' || *p == ';'){
       add_token(v,*p,p);
       i++;
       p++;
@@ -67,6 +78,13 @@ Vector *tokenize(char *p){
       Token *t = add_token(v,TK_NUM,p);
       t->val = strtol(p,&p,10);
       i++;
+      continue;
+    }
+    if('a' <= *p && *p <= 'z'){
+      Token *t = add_token(v,TK_IDENT,p);
+      t->name = *p;
+      i++;
+      p++;
       continue;
     }
 
@@ -80,13 +98,15 @@ Vector *tokenize(char *p){
 
 enum{
   ND_NUM = 256,
+  ND_IDENT,
 };
 
 typedef struct Node {
-  int ty;
-  struct Node *lhs;
-  struct Node *rhs;
-  int val;
+  int ty;//type
+  struct Node *lhs; //Left Hand Side
+  struct Node *rhs; // Right Hand Side
+  int val; // used when ty == ND_NUM
+  char name; // used when ty == ND_IDENT
 } Node;
 
 Node *new_node(int ty, Node *lhs, Node *rhs){
@@ -104,6 +124,13 @@ Node *new_node_num(int val){
   return node;
 }
 
+Node *new_node_id(char name){
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_IDENT;
+  node->name = name;
+  return node;
+}
+
 
 int consume(int ty){
   if (((Token *)tokens->data[pos])->ty != ty)
@@ -111,9 +138,36 @@ int consume(int ty){
   pos++;
   return 1;
 }
+Node *stmt();
+Node *assign();
 Node *add();
 Node *mul();
 Node *term();
+
+Node *code[100];
+void program() {
+  int i=0;
+  while(((Token *)tokens->data[pos])->ty != TK_EOF){
+    code[i++] = stmt();
+  }
+  code[i] = NULL;
+}
+
+Node *stmt() {
+  Node *node = assign();
+  if (!consume(';'))error(((Token *)tokens->data[pos])->input);
+  return node;
+}
+
+Node *assign(){
+  Node *node = add();
+  for(;;){
+    if(consume('='))
+      node = new_node('=', node, assign());
+    else
+      return node;
+  }
+}
 
 Node *add(){
   Node *node = mul();
@@ -151,14 +205,44 @@ Node *term(){
   }
   if(t->ty == TK_NUM)
     return new_node_num(((Token *)tokens->data[pos++])->val);
+  if(t->ty == TK_IDENT){
+    return new_node_id(((Token *)tokens->data[pos++])->name);
+  }
 
   fprintf(stderr,"unknown token : %s", t->input);
 }
+
+void gen_lval(Node *node){
+  if(node->ty != ND_IDENT)error(node->val);
+
+  int offset = ('z' - node->name + 1) * 8;
+  printf("  mov rax, rbp\n");
+  printf("  sub rax, %d\n",offset);
+  printf("  push rax\n");
+}
+
 
 
 void gen(Node *node){
   if(node->ty == ND_NUM){
     printf("  push %d\n", node->val);
+    return;
+  }
+  if(node->ty == ND_IDENT){
+    gen_lval(node);
+    printf("  pop rax\n");
+    printf("  mov rax, [rax]\n");
+    printf("  push rax\n");
+    return;
+  }
+  if(node->ty == '='){
+    gen_lval(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    printf("  mov [rax], rdi\n");
+    printf("  push rdi\n");
     return;
   }
   gen(node->lhs);
@@ -185,11 +269,7 @@ void gen(Node *node){
 }
 
 
-// func to report error
-void error(int i){
-  fprintf(stderr, "unknown string: '%s'\n",((Token *)tokens->data[i])->input);
-  exit(1);
-}
+
 
 
 int main(int argc, char **argv){
@@ -197,17 +277,32 @@ int main(int argc, char **argv){
     fprintf(stderr, "wrong argument's number\n");
     return 1;
   }
+
+  //tokenize and parse
+  // result will be kept in code
   tokens = tokenize(argv[1]);
-  Node *node = add();
+  program();
 
   //output initial part of asm
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
+
+  //prologue: reserve 26 variants' space
+  printf("  push rbp\n");
+  printf("  mov rbp, rsp\n");
+  printf("  sub rsp, 208\n");
   // generate code descending AST
-  gen(node);
-  //take value from stacktop
-  printf("  pop rax\n");
+  for (int i=0;code[i];i++){
+    gen(code[i]);
+    //stacktop is result of equation
+    //pop it not to be stackoverflow
+    printf("  pop rax\n");
+  }
+
+  //epilogue
+  printf("  mov rsp, rbp\n");
+  printf("  pop rbp\n");
   printf("  ret\n");
   return 0;
 }
